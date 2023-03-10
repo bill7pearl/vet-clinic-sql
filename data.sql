bfb730be34289b2717114c3e658a73784a639f43
Billal <billal7chami@gmail.com>
2023-03-10 23:24:34 +0100

    Finish all sql UPDATE, QUeries

--- data.sql
+++ data.sql
@@ -11,5 +11,27 @@ INSERT INTO animals VALUES  (6, 'Charmander', 'Feb 8, 2020', 0, 'false', -11),
                             (9, 'Angemon', 'Jun 12, 2005', 1, 'true', -45),
                             (10, 'Boarmon', 'Jun 7, 2005', 7, 'true', 20.4),
                             (11, 'Blossom', 'Oct 13, 1998', 3, 'true', 17),
-                            (12, 'Ditto', 'May 14, 2022', 4, 'true', 22)
-                            ;
+                            (12, 'Ditto', 'May 14, 2022', 4, 'true', 22);
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34),('Jennifer Orwell', 19),
+                                           ('Bob', 45),
+                                           ('Melody', 77),
+                                           ('Dean Winchester', 14),
+                                           ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon'),
+                                  ('Digimon');
+
+UPDATE animals SET species_id = species.id FROM species WHERE species.name = 'Digimon' AND animals.name LIKE '%mon';
+
+UPDATE animals SET species_id = species.id FROM species WHERE species.name = 'Pokemon' AND animals.species_id is NULL;
+
+UPDATE animals SET owner_id = owners.id FROM owners WHERE animals.name = 'Agumon' AND owners.full_name = 'Sam Smith';
+
+UPDATE animals SET owner_id = owners.id FROM owners WHERE animals.name IN ('Gabumon', 'Pikachu') AND owners.full_name = 'Jennifer Orwell';
+
+UPDATE animals SET owner_id = owners.id FROM owners WHERE animals.name IN ('Charmander', 'Squirtle', 'Blossom') AND owners.full_name = 'Melody Pond';
+
+UPDATE animals SET owner_id = owners.id FROM owners WHERE animals.name IN ('Angemon', 'Boarmon') AND owners.full_name = 'Dean Winchester';
+
+UPDATE animals SET owner_id = owners.id FROM owners WHERE animals.name IN ('Devimon', 'Plantmon') AND owners.full_name = 'Bob';
--- schema.sql
+++ schema.sql
@@ -1,4 +1,18 @@
 /* Database schema to keep the structure of entire database. */
 
- CREATE TABLE animals (id INT, name CHAR(250), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
- ALTER TABLE animals ADD COLUMN species CHAR(250);
+CREATE TABLE animals (id INT, name CHAR(250), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
+ALTER TABLE animals ADD COLUMN species CHAR(250);
+
+CREATE TABLE owners (id SERIAL PRIMARY KEY, full_name CHAR(250) NOT NULL, age INT);
+
+CREATE TABLE species (id SERIAL PRIMARY KEY, name CHAR(250) NOT NULL);
+
+ALTER TABLE animals DROP COLUMN id;
+
+ALTER TABLE animals ADD COLUMN id SERIAL PRIMARY KEY;
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT, ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
