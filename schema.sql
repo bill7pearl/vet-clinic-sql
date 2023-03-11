b84357a88cfd8dfa71baac8ad487eec8b9b26085
Billal <billal7chami@gmail.com>
2023-03-11 16:51:10 +0100

    Finish all INSERT, relations SQL

--- schema.sql
+++ schema.sql
@@ -16,3 +16,9 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id);
 
 ALTER TABLE animals ADD COLUMN owner_id INT, ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+CREATE TABLE vets (id serial PRIMARY KEY, name VARCHAR(100), age INT, date_of_graduation DATE);
+
+CREATE TABLE specializations (species_id INT REFERENCES species(id), vet_id INT REFERENCES vets(id));
+
+CREATE TABLE visits (animal_id INT REFERENCES animals(id), vet_id INT REFERENCES vets(id), visited_date date NOT NULL DEFAULT CURRENT_DATE);
